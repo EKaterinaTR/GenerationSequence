c729c9640e2ebaa5734534e5fec24eb670e86a33
EKaterinaTR <64559487+EKaterinaTR@users.noreply.github.com>
2020-05-20 20:20:24 +0300

    Update Tests.java

--- Tests.java
+++ Tests.java
@@ -5,11 +5,11 @@ public class Tests {
     String [] strings = {"1","11","21","1211","111221","312211"};
 
     @Test
-    public void CorrectGenerate6Members() {
+    public void CorrectGenerate6Members () {
         SequenceGenerator sg = new SequenceGenerator();
         String [] strings1 = sg.getSequence();
 
-        for(int i = 0; i < strings1.length; i++) {
+        for (int i = 0; i < strings1.length; i++) {
             Assert.assertEquals(true,strings1[i].equals(strings[i]));
         }
 
