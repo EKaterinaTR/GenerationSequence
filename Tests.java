f9f8cbe8568cbe0a9eae788a2108fc78db271c0a
EKaterinaTR <64559487+EKaterinaTR@users.noreply.github.com>
2020-05-21 17:22:34 +0300

    Tests for every member from 1 to 6

--- Tests.java
+++ Tests.java
@@ -2,16 +2,54 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Tests {
-    String [] strings = {"1","11","21","1211","111221","312211"};
+    String[] strings = {"1", "11", "21", "1211", "111221", "312211"};
 
     @Test
-    public void CorrectGenerate6Members () {
+    public void CorrectGenerate1Member () {
         SequenceGenerator sg = new SequenceGenerator();
-        String [] strings1 = sg.getSequence();
+        String[] strings1 = sg.getSequence();
 
-        for (int i = 0; i < strings1.length; i++) {
-            Assert.assertEquals(true,strings1[i].equals(strings[i]));
-        }
-
+        Assert.assertEquals(true, strings1[0].equals(strings[0]));
     }
+
+    @Test
+    public void CorrectGenerate2Member () {
+        SequenceGenerator sg = new SequenceGenerator();
+        String[] strings1 = sg.getSequence();
+
+        Assert.assertEquals(true, strings1[1].equals(strings[1]));
+    }
+    @Test
+    public void CorrectGenerate3Member () {
+        SequenceGenerator sg = new SequenceGenerator();
+        String[] strings1 = sg.getSequence();
+
+        Assert.assertEquals(true, strings1[2].equals(strings[2]));
+    }
+
+    @Test
+    public void CorrectGenerate4Member () {
+        SequenceGenerator sg = new SequenceGenerator();
+        String[] strings1 = sg.getSequence();
+
+        Assert.assertEquals(true, strings1[3].equals(strings[3]));
+    }
+
+    @Test
+    public void CorrectGenerate5Member () {
+        SequenceGenerator sg = new SequenceGenerator();
+        String[] strings1 = sg.getSequence();
+
+        Assert.assertEquals(true, strings1[4].equals(strings[4]));
+    }
+
+    @Test
+    public void CorrectGenerate6Member () {
+        SequenceGenerator sg = new SequenceGenerator();
+        String[] strings1 = sg.getSequence();
+
+        Assert.assertEquals(true, strings1[5].equals(strings[5]));
+    }
+
+
 }
